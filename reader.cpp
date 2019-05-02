26b6c33ed8c130b61989afe4bbec5f3d52febc92
Raymond Firebaugh <ray.firebaugh@gmail.com>
2019-05-02 18:29:23 -0500

    Added reader.cpp includes and methods

--- reader.cpp
+++ reader.cpp
@@ -1,1 +1,14 @@
 #include "csv.h"
+#include "reader.hpp"
+#include "student.hpp"
+#include "instruments.hpp"
+#include <string>
+#include <vector>
+
+static std::vector<Student> getStudentList(std::string fileName, std::vector<std::string> gigList) {
+    
+}
+
+static std::vector<std::string> getGigList(std::string fileName) {
+    
+}
