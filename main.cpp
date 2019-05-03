929b2a6c0ffdf858bbe3cc2495547e49d2b77da1
StormCountZero <40500487+StormCountZero@users.noreply.github.com>
2019-05-03 18:13:45 -0500

    Added basic beginning functionality to main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "student.hpp"
 #include "reader.hpp"
+#include <iostream>
 
 int main()
 {
+    std::vector<std::string> gigList;   // Vector of gigs to be scheduled
+    std::vector<Student> students;      // Vector of students who have given availability
+
     // Prompt for gig survey file and vc file
+    std::cout << "Gig List File:";
+
     // Call getGigList and getStudentList
     // Prompt to choose gig
     // Prompt for instrumentation, either default file or give specific numbers
