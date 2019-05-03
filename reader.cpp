05712e38d1eb48976be1831b32a9e63834a60d04
Raymond Firebaugh <ray.firebaugh@gmail.com>
2019-05-03 17:56:51 -0500

    Save reader.cpp to merge

--- reader.cpp
+++ reader.cpp
@@ -6,7 +6,9 @@
 #include <vector>
 
 static std::vector<Student> getStudentList(std::string fileName, std::vector<std::string> gigList) {
+    std::vector<Student> studentList; //Create student vector
     
+
 }
 
 static std::vector<std::string> getGigList(std::string fileName) {
