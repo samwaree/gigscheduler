48facf3eb37377cd6e6c979aea6df8b7599dc874
StormCountZero <40500487+StormCountZero@users.noreply.github.com>
2019-05-04 13:14:12 -0500

    Adding untested main functionality and fixing merge conflicts in reader.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,31 @@
 
 int main()
 {
+    std::string gigSurvey;              // User input path to gig survey file
+    std::string vc;                     // User input path to vc file
     std::vector<std::string> gigList;   // Vector of gigs to be scheduled
     std::vector<Student> students;      // Vector of students who have given availability
 
-    // Prompt for gig survey file and vc file
-    std::cout << "Gig List File:";
+
+    // Initial welcome message and prompt for gig survey file and vc file
+    std::cout << "Gig Survey File: ";
+    std::cin >> gigSurvey;
+    std::cout << "VC File: ";
+    std::cin >> vc;
 
     // Call getGigList and getStudentList
+    gigList = Reader::getGigList(gigSurvey);
+    students = Reader::getStudentList(gigSurvey, gigList);
+
     // Prompt to choose gig
+    int choice;
+    std::cout << "Select the gig you wish to schedule:" << std::endl;
+    // indexed for loop so user can pick the index of the gig they want to schedule
+    for (int i = 0; i < gigList.size(); i++) {
+        std::cout << i + 1 << " " << gigList.at(i);
+    }
+    std::cin >> choice;
+    std::cout  << gigList.at(choice);
     // Prompt for instrumentation, either default file or give specific numbers
     // Do some magic to get gig list
     // Print gig list and save to file
