5589d1734a28863fa4c1422b3b4848deab38ee77
Raymond Firebaugh <ray.firebaugh@gmail.com>
2019-05-04 17:46:03 -0500

    Removed gigList parameter from getStudentList and implemented getStudentList without VC or Error handling

--- reader.cpp
+++ reader.cpp
@@ -1,22 +1,74 @@
-#include "csv.h"
+//#include "csv.h"
 #include "parser.hpp"
 #include "reader.hpp"
 #include "student.hpp"
 #include "instruments.hpp"
 #include <string>
 #include <vector>
+#include <map>
 #include <iostream>
 
+const int FIRST_DATA_ROW = 3;
+const int NAME_COL = 18;
+const int INSTRUMENT_COL = 20;
+const int FIRST_GIG_COL = 24;
+const int NUM_EXTRA_COLS = 2;
 
-std::vector<Student> Reader::getStudentList(std::string gigFileName, std::string vcFileName, std::vector<std::string> gigList)
+Instrument getEnumFromString(std::string instrumentString) {
+    // Define a static dictionary to convert instrument strings into instruments
+    static std::map<std::string, Instrument> enumMap {
+        {"Trumpet 1", TRUMPET1},
+        {"Trumpet 2", TRUMPET2},
+        {"Trumpet 3", TRUMPET3},
+        {"Trombone 1", BONE1},
+        {"Trombone 2", BONE2},
+        {"Trombone 3", BONE3},
+        {"Tuba", TUBA},
+        {"Drums", DRUMS},
+        {"Other", OTHER}
+    };
+
+    return enumMap[instrumentString];
+
+}
+
+int getNumResponse(std::string response) {
+    if (response == "Yes")
+    {
+        return 1;
+    }
+    else if (response == "No")
+    {
+        return 0;
+    }
+    else //For Testing
+    {
+        // Parsing failed
+        return -1;
+    }
+}
+
+std::vector<Student> Reader::getStudentList(std::string gigFileName, std::string vcFileName)
 {
-    std::vector<Student> vect;
+    std::vector<Student> studentVector;
     std::ifstream gigStream(gigFileName);
     aria::csv::CsvParser gigParser(gigStream);
 
+    int rowCount = 0;
+    for (auto &row : gigParser)
+    {
+        if (rowCount >= FIRST_DATA_ROW) 
+        {
+            std::vector<int> availability;
+            for (int i = FIRST_GIG_COL; i < row.size() - NUM_EXTRA_COLS; i++) {
+                availability.push_back(getNumResponse(row[i]));
+            }
+            studentVector.push_back(Student(row[NAME_COL], 0, getEnumFromString(row[INSTRUMENT_COL]), availability));
+        }
+        rowCount++;
+    }
 
-
-    return vect;
+    return studentVector;
 }
 
 std::vector<std::string> Reader::getGigList(std::string fileName)
